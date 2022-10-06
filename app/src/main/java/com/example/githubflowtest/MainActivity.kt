dc64c6ffeafffd74de146227e1b316f5fd0004a0
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-10-06 13:15:25 +0200

    first commit

---
+++ app/src/main/java/com/example/githubflowtest/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.githubflowtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+//en person skapar projektet
+//samma person gör en git init
+//samma person skapar ett projekt på github
